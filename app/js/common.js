1d290961e93d5244701b5f86a7fbc5ff3e9b99f9
StarkOne <Stark.pro40@gmail.com>
2017-03-16 14:15:03 +0200

    first-commit

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$(function() {
+
+	$(".js-featured__slider").bxSlider({
+		hideControlOnEnd: true,
+		minSlides: 1,
+		maxSlides: 3,
+		slideWidth: 360,
+		slideMargin: 20,
+		pager: false,
+		infiniteLoop: false
+	});
+});
